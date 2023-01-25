659a193ef184067b0a8ec7a415a9bec03a5d00cb
psadi <adithya3494@gmail.com>
2023-01-25 11:43:40 +0530

    2023-01-25 11:43:40 dotfiles updates
    Former-commit-id: 684881852c694e50fdbc410ea39a3c40af9d4f52 [formerly 684881852c694e50fdbc410ea39a3c40af9d4f52 [formerly ce6c9c57ce0d8c1b0619becbf6521d587abf6156]]
    Former-commit-id: 07111fdf275d6798d42de6dbd5653e63481967a0
    Former-commit-id: cc9a890f8c570dd4719276dcbd0fae3fc8fa14c0
    Former-commit-id: 6ebd59395fa4856ac7f971f63738e51d732a8007
    Former-commit-id: d0240f1b6dae20ede042c7a6d0cbc64121e385e5
    Former-commit-id: 49634d52c4a89444fc833f4a2470e0a9d15e1f4e
    Former-commit-id: 7977d8eaca1c2bf4113524193260b24994271530

--- zsh/.zsh/_zsh.zsh
+++ zsh/.zsh/_zsh.zsh
@@ -7,11 +7,14 @@ export _ZSH_PLUGINS_PATH="${_ZSH_CONFIG_PATH}/plugins"
 
 # ZSH PLUGINS
 # -----------
+
+source "${_ZSH_CONFIG_PATH}/zsh-defer/zsh-defer.plugin.zsh"
+
 for z in `ls "${_ZSH_PLUGINS_PATH}"`;
 do
 	if [ -d "${_ZSH_PLUGINS_PATH}/${z}" ];
 	then
-		source "${_ZSH_PLUGINS_PATH}/${z}/${z}.zsh"
+		zsh-defer "${_ZSH_PLUGINS_PATH}/${z}/${z}.zsh"
 	fi
 done
 
