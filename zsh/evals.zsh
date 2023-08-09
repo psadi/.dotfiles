160fb81d47e2887aa84d0eed90362bd2bda735b1
psadi <adithya3494@gmail.com>
2023-08-09 22:12:18 +0530

    2023-08-09 22:12:18 dotfiles updates
    Former-commit-id: 98ea838a6c69b7b3b3becd0b480c705b7d3880d9 [formerly 7b62dfef736a49d24891192a43613f69c54d6721] [formerly 7b62dfef736a49d24891192a43613f69c54d6721 [formerly 6d797c674ceb49623ed23eaa896a0601d75bc909]]
    Former-commit-id: ade1254a2a51d875cb0c399385be5dfcc39ce6c3
    Former-commit-id: c4175e421bbcd6ef4e7b36d2996b1ba0134028c6
    Former-commit-id: 1053187e0633f79afea39786f65bf098fafccea8
    Former-commit-id: 1af8faca39ff8bcd6e5a7eaaa93d6f3b20123c6e
    Former-commit-id: 9eecdfdb7cfff860233bce4fff817a6dbb286771
    Former-commit-id: 851fc1998bde3fa45841d0448a3188a148953543

--- zsh/evals.zsh
+++ zsh/evals.zsh
@@ -5,5 +5,6 @@ then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 fi
 
-# eval "$(starship init zsh)"
+eval "$(starship init zsh)"
+# export PS1='%F{blue}%~ %(?.%F{green}.%F{red})%#%f '
 eval "$(zoxide init zsh)"
