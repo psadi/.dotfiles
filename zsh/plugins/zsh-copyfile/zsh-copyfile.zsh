06c01866c5fba85a390c3f8f8046e8be7c7575e9
psadi <adithya3494@gmail.com>
2023-08-15 23:06:15 +0530

    2023-08-15 23:06:15 dotfiles updates
    Former-commit-id: 2a1847346bb21bb97eb95136159245f9647844a4 [formerly 66ea289112d1ec8076a8c4727e667310d9853834] [formerly 66ea289112d1ec8076a8c4727e667310d9853834 [formerly 4fcf6eb67fb87d55059cb31e0bb26591c4a04b39]]
    Former-commit-id: 4833b570fe47af80ca635c6a85a10229c9345efb
    Former-commit-id: 594e0130ff55d4a6e356928a65b0d7e650c31330
    Former-commit-id: b8107f5e4eac8a194603484125f2ec38ea008a0b
    Former-commit-id: 3d7af3bdc6cece4f9e7294ee19899a20137d338a
    Former-commit-id: 1b673a537900cd940ed9ee42242b9f268b4bbf28
    Former-commit-id: c5e5002e02b1825f5ecb7fb3bdf3ccfd73de6ccd

--- zsh/plugins/zsh-copyfile/zsh-copyfile.zsh
+++ zsh/plugins/zsh-copyfile/zsh-copyfile.zsh
@@ -3,7 +3,8 @@
 # copyfile <file>
 function copyfile {
   emulate -L zsh
-  case "$(uname -a | awk '{print $1}')" in
+  
+  case "${OSTYPE}" in
     "Linux") clipcopy $1 ;;
     "Darwin") cat $1 | pbcopy ;;
     *) echo "unsupported ostype" ;;
