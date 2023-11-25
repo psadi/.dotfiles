86f6d7da338c0dc28255183f512c1325a13abc28
psadi <adithya3494@gmail.com>
2023-11-26 04:14:55 +0530

    2023-11-26 04:14:55 dotfiles updates
    Former-commit-id: 0366d6f1b1624273f3f88c68e49fb734347a7797 [formerly 18029cb6a116318059d51d9468cb666c0d729471] [formerly 18029cb6a116318059d51d9468cb666c0d729471 [formerly a762b8728ea7b02a03e45da3acbe991916be785d]]
    Former-commit-id: e528897bbbbfca41133e5699fd135f07837f6054
    Former-commit-id: 563b6ab023a8d4aa1b5208f25fb986443253896d
    Former-commit-id: 6a470a6f8cb76115be53d66540c371ff9319a07d
    Former-commit-id: 49428e2ec20502d3812830921c05fd6dff23d611
    Former-commit-id: fb3d98d57da3a5955644ceaed6779964fadde6b1
    Former-commit-id: 0500b891ba525d77e2bfdb1725de825c11b7537d

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -28,3 +28,15 @@ lxl(){
         "/Applications/Lite XL.app/Contents/MacOS/lite-xl" &
     fi
 }
+
+
+function mkcd {
+  last=$(eval "echo \$$#")
+  if [ ! -n "$last" ]; then
+    echo "Enter a directory name"
+  elif [ -d $last ]; then
+    echo "\`$last' already exists"
+  else
+    mkdir $@ && cd $last
+  fi
+}
