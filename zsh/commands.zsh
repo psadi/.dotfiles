4a30420c8902cd14d6a9d6e6022bc69b2721fd64
psadi <adithya3494@gmail.com>
2023-12-19 01:00:38 +0530

    2023-12-19 01:00:38 dotfiles updates
    Former-commit-id: 15f2a8a0a38bb0b80fa958b55412890c189e9fa6 [formerly 21772c34cbf490d1101d2ca1ac7066300ac3d8b6] [formerly 21772c34cbf490d1101d2ca1ac7066300ac3d8b6 [formerly a4daf44a4447bc3b8f6ac7872b0f1d0a1a5b298d]]
    Former-commit-id: 212e28456611c88c385a468114515ac6f6145e97
    Former-commit-id: 76ea44106a6ffa19a6af180487e5bbc312cc5afd
    Former-commit-id: 47f8f77a824a7b7c8bdb7277e0338fb513c4aaa9
    Former-commit-id: 3cc74a228a08333f74fbc32724c62b193c0765ca
    Former-commit-id: 6f5d0809ec88d3a479d65a46919c1a8d7134daf9
    Former-commit-id: 38ade249cd14d38c1946029cf721e32627395df0

--- zsh/commands.zsh
+++ zsh/commands.zsh
@@ -6,11 +6,6 @@ then
     export MANPAGER='bat'
 fi
 
-if ! type "hx --version" > /dev/null; then
-  export EDITOR=hx
-	export editor=hx
-fi
-
 if [ -d "$HOME/.cargo" ]
 then
 	source "$HOME/.cargo/env"
