4cd1f58cbdb18da74606b2be70a419dc9ae0b739
psadi <ps.adithya@icloud.com>
2024-01-29 16:15:11 +0530

    2024-01-29 16:15:11 dotfiles updates
    Former-commit-id: 5ffac723150ad0f98307293e33107a5d5e5205b5 [formerly 5ffac723150ad0f98307293e33107a5d5e5205b5 [formerly 8b3317ae5012aff4b0542b5483be9bdb4c864b91]]
    Former-commit-id: 8bd001847857cc2f04cc3beca23335f5294f1361
    Former-commit-id: 795708240b8cab31e28efa4aa38f2bc50d6c9735
    Former-commit-id: 4efa3069e619994ce49c04e8aa3e0f87fd7c91f3
    Former-commit-id: 496958c57beb631bda61d8842f20c13d1112ff87
    Former-commit-id: e8acbb8a664eb3e85c6d6987682b76ddbffe503f
    Former-commit-id: 871831a1b42ec6fe3c8151af40beb4879c725a9c

--- zsh/zsh/cmd/command.zsh
+++ zsh/zsh/cmd/command.zsh
@@ -9,3 +9,9 @@ if command -v bat &> /dev/null; then
   alias cat="bat -p"
   export MANPAGER="bat"
 fi
+
+if [ -d /opt/nvim-linux64 ]; then
+  export PATH=$PATH:/opt/nvim-linux64/bin
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/nvim-linux64/lib
+  export MANPATH=$MANPATH:/opt/nvim-linux64/man
+fi
