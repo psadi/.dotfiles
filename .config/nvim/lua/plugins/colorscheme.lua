d1d5e881ea794cf346f37f11055a4823c471b3b9
psadi <ps.adithya@icloud.com>
2024-02-17 00:23:01 +0530

    2024-02-17 00:23:01 dotfiles updates
    Former-commit-id: ce11993f927418e403080231a59e3d0b9649507c [formerly ce11993f927418e403080231a59e3d0b9649507c [formerly b60872bae7e2ab0cdde961a103cd4c18dfaaa236]]
    Former-commit-id: 9a534d2080a31290a5837337eb4ef0fce8122936
    Former-commit-id: db305133a69bdf7aa998f31c59e2c79868891e6e
    Former-commit-id: 7cef611f9ef2130b62a6314caf4b167600ce9f2b
    Former-commit-id: 805a68a6ce88593e630fe43cb85ec2149aa73c33
    Former-commit-id: d884fffcbd2da75125cd6ab99dd3174ae9cac9e0
    Former-commit-id: 592e61f63c613cf7eb6ba09c6949b694d22460e5

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,17 @@
 return {
-  "baliestri/aura-theme",
-  lazy = false,
-  priority = 1000,
-  config = function(plugin)
-    vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-    vim.cmd([[colorscheme aura-dark]])
-  end,
+  { "Mofiqul/vscode.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "vscode",
+    },
+  },
+
+  -- "baliestri/aura-theme",
+  -- lazy = false,
+  -- priority = 1000,
+  -- config = function(plugin)
+  --   vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
+  --   vim.cmd([[colorscheme aura-dark]])
+  -- end,
 }
