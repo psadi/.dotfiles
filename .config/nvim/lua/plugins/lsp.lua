be81edb6a64cac082e11cd5df3aba2e8d2fd2592
psadi <ps.adithya@icloud.com>
2024-05-24 12:22:07 +0530

    2024-05-24 12:22:07 dotfiles updates
    Former-commit-id: c194447f96a61717687e3df6e1e94a4865f53326
    Former-commit-id: ad39c0afe10b4385dfe807847884fc13a031d97c
    Former-commit-id: 129ac87e7e5b8e1abcb47055903f17068dedc015
    Former-commit-id: 57e46b8ef2543e142d440ada54cc50a5180d03b3
    Former-commit-id: cbf86d72ee0dab03b1917a9091ec56af99371ba3
    Former-commit-id: cbb323398c95d9e59de701767a95ab16ba47bfaa
    Former-commit-id: e3c2c11baee9f5866697d4213a781a33af0d0c11

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup {
-        ensure_installed = { "lua_ls", "ruff", "ruff_lsp", "zls" },
+        ensure_installed = { "lua_ls", "ruff_lsp", "zls" },
       }
     end
   },
