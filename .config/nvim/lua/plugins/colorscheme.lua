7ac04c813ae5dcb77fef6acfebaba8bdbb8bdd8e
psadi <ps.adithya@icloud.com>
2024-07-07 00:21:28 +0530

    2024-07-07 00:21:28 dotfiles updates
    Former-commit-id: 899af3f415146e06f3a81f2b14552a5c726bfa50
    Former-commit-id: b39ee8d1d6a21bfaa5dabed11ced52d6630f3cef
    Former-commit-id: 71c66c8f230443b88d2786f1fa930cd8ee7da51c
    Former-commit-id: 1a84b0b4bc50ca59ca27407672661e9a2fa008d1
    Former-commit-id: 3eadad44d00dfa67007b186ea8df5058a7da1961
    Former-commit-id: f5d11aa5d09d1c9136931e884ad3f1bbdca9dcec
    Former-commit-id: 82b0e6a4ffba1f01787299a9082987e9f0b6e753

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,16 +1,8 @@
 return {
-  -- "lunarvim/darkplus.nvim",
-  -- name = "darkplus",
-  -- priority = 1000,
-  -- config = function()
-  --   vim.cmd.colorscheme "darkplus"
-  -- end
-    "baliestri/aura-theme",
-    lazy = false,
-    priority = 1000,
-    config = function(plugin)
-      vim.opt.rtp:append(plugin.dir .. "/packages/neovim")
-      vim.cmd([[colorscheme aura-dark]])
-    end
-
+  "lunarvim/darkplus.nvim",
+  name = "darkplus",
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme "darkplus"
+  end
 }
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,12 +1,22 @@
--- ------------
+-- ----------
 -- Initialize
--- ------------
+-- ----------
 local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+-- ---------------------------------
+-- MacOS Start with GUI as maximized
+-- ---------------------------------
+wezterm.on("gui-startup", function()
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
+end)
+
 local config = wezterm.config_builder()
 
--- ------------
+-- ----
 -- Font
--- ------------
+-- ----
 config.font = wezterm.font("MonoLisa Nerd Font", { weight = "Regular", italic = false })
 config.font_size = 17
 
@@ -48,18 +58,24 @@ config.color_schemes = {
 	},
 }
 
--- ------------
+-- --------------------
 -- Tabs, Windows & More
--- ------------
+-- --------------------
 config.enable_tab_bar = true
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.tab_max_width = 20
 config.show_new_tab_button_in_tab_bar = false
-
 config.clean_exit_codes = { 130 }
 config.enable_scroll_bar = true
-
 config.window_close_confirmation = "NeverPrompt"
+
+-- -----------------------------------------------------
+-- MacOS navigation between words using Opt + Arrow Keys
+-- -----------------------------------------------------
+config.keys = {
+    {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+    {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+  }
 
 return config
