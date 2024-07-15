4a3ae4ae67f934ee390b9f946d61962c001a5fc4
psadi <ps.adithya@icloud.com>
2024-07-15 17:56:08 +0530

    2024-07-15 17:56:08 dotfiles updates
    Former-commit-id: b8d951185753b4908f19a15651e1d374ab8017c8
    Former-commit-id: 51cd3bfbd75ddbfb25257ddc24b221d47ef9c08e
    Former-commit-id: 272931e6d11507e91e83df45ece0adb0b42cfbd2
    Former-commit-id: 7c037fb61dd7c14ddcf0e15a69022f8279dc8374
    Former-commit-id: 6a58d980628292c651449bd90b6dafe65996497c
    Former-commit-id: 4d33ae95e9b1a4465dee50ade5c56f733c07769d
    Former-commit-id: e7e9dc7fecd71f41e841c4b02638addbebc36646

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -3,5 +3,10 @@ return {
   { "telescope-fzf-native.nvim", enabled = false },
   { "nvimdev/dashboard-nvim", enabled = false },
   { "folke/which-key.nvim", enabled = false },
+  { "folke/noice.nvim", enabled = false },
   { "nvim-neo-tree/neo-tree.nvim", enabled = false },
+  { "folke/neodev.nvim", enabled = false },
+  { "catppuccin/nvim", enabled = false },
+  { "folke/todo-comments.nvim", enabled = false },
+  { "Bilal2453/luvit-meta", enabled = false },
 }
