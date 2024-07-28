9818392cd78b850dbe3b1ade1e15ab46fbd56a94
psadi <ps.adithya@icloud.com>
2024-07-29 00:39:21 +0530

    2024-07-29 00:39:21 dotfiles updates
    Former-commit-id: daf9cee845b90d90bdde9393b08188122f3da21e
    Former-commit-id: 396e2bc7311c89ee01302514db06c36aaa7cb522
    Former-commit-id: 5de696f73395744bafe538d8bd47a0e3ea139349
    Former-commit-id: fdaff32cf4e32a2d2d480a2576a254dee66ce872
    Former-commit-id: 862d0a568f2eb27a09e5f856afbb466b1e12d4d5
    Former-commit-id: a4a3c34a353be8e6ec94b15b7c828d4ae661e4bf
    Former-commit-id: 2c28116fc23da4bfc2ee51dad4e91516142e743e

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,89 +3,44 @@
 -- ----------
 local wezterm = require("wezterm")
 local mux = wezterm.mux
+local config = wezterm.config_builder()
 
 -- ---------------------------------
 -- MacOS Start with GUI as maximized
 -- ---------------------------------
 wezterm.on("gui-startup", function()
-	local tab, pane, window = mux.spawn_window(cmd or {})
-	window:gui_window():maximize()
+  local tab, pane, window = mux.spawn_window(cmd or {})
+  window:gui_window():maximize()
 end)
 
-local config = wezterm.config_builder()
 
--- ----
--- Font
--- ----
+-- --------------------
+-- Configs
+-- --------------------
 config.font = wezterm.font("MonoLisa Nerd Font")
 config.font_size = 19
-
--- ------------
--- Color Scheme
--- ------------
 config.color_scheme = "GruvboxDarkHard"
-config.color_schemes = {
-	["Dark+"] = {
-		foreground = "#f8f8f8",
-		background = "#1e1e1e",
-		cursor_bg = "#aeafad",
-		cursor_fg = "#1e1e1e",
-		cursor_border = "#52ad70",
-		selection_fg = "black",
-		selection_bg = "#2e4e75",
-		scrollbar_thumb = "#4f4f4f",
-		ansi = {
-			-- With reference to https://wezfurlong.org/wezterm/config/appearance.html
-			"#000000", -- black
-			"#e06c75", -- maroon
-			"#98c379", -- green
-			"#ffcb6b", -- olive
-			"#82aaff", -- navy
-			"#ff79c6", --purple
-			"#89ddff", -- teal
-			"#ffffff", -- silver
-		},
-		brights = {
-			"#5c5c5c", -- grey
-			"#ff5370", -- red
-			"#c3e88d", -- lime
-			"#ffcb6b", -- yellow
-			"#61afef", -- blue
-			"#ff79c6", -- fuchsia
-			"#89ddff", -- aqua
-			"#ffffff", -- white
-		},
-	},
-}
-
--- --------------------
--- Tabs, Windows & More
--- --------------------
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 config.tab_bar_at_bottom = true
 config.use_fancy_tab_bar = false
 config.tab_max_width = 20
 config.show_new_tab_button_in_tab_bar = false
-config.clean_exit_codes = { 130 }
 config.enable_scroll_bar = false
--- config.window_close_confirmation = "NeverPrompt"
 config.window_background_opacity = 10
 config.macos_window_background_blur = 10
-config.window_decorations = "RESIZE | TITLE"
-
--- config.window_padding = {
---   left = 0,
---   right = 0,
---   top = 0,
---   bottom = 0,
--- }
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
 
 -- -----------------------------------------------------
 -- MacOS navigation between words using Opt + Arrow Keys
 -- -----------------------------------------------------
 config.keys = {
-	{ key = "LeftArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
-	{ key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+  { key = "LeftArrow",  mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
+  { key = "RightArrow", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
 }
 
 return config
