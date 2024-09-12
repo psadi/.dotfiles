562f6f535a88f5c02a17230f65452ca71da63613
psadi <ps.adithya@icloud.com>
2024-09-12 20:35:26 +0530

    2024-09-12 20:35:26 dotfiles updates
    Former-commit-id: 74bcec80e1cf78f37d031497849e8ef97f20f022
    Former-commit-id: 95396af3a6ed617fb88253097e9f7fdca70d44de
    Former-commit-id: f8de1e6531da60163f5c29c3e036fbad21a62e64
    Former-commit-id: e88a2415068a6437fd5b4c932da13e685c7468cd
    Former-commit-id: 44eefd4a86eb247ee327d30c220bf826222bac87
    Former-commit-id: d4b273527cd85bb0ec4097e5f8024ecc97a42ea1
    Former-commit-id: 7f9cc27d66904caffd57702e94a57ae0d4019e52

--- zsh/command.zsh
+++ zsh/command.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+# Bat
+#---------------------------------------------
 if (( $+commands[bat] )); then
   alias cat="bat -p"
   export MANPAGER="bat"
@@ -8,17 +10,21 @@ if (( $+commands[bat] )); then
   export FZF_CTRL_T_OPTS="--preview '$show_file_or_dir_preview'"
 fi
 
+# K9s
+#---------------------------------------------
 if (( $+commands[k9s] )); then
   alias k9s="k9s --logoless --headless --readonly --crumbsless"
 fi
 
+# Eza
+#---------------------------------------------
 if (( $+commands[eza] )); then
   alias ls='eza --group-directories-first --icons'
   alias ll='ls -lh --git'
   alias la="ll -a"
   alias lt="ll --tree --level=2"
   export FZF_ALT_C_OPTS="--preview 'eza --tree --color=always {} | head -200'"
-else
+else # fallback 
   alias ls="ls --color=auto"
   alias ll="ls -l -go"
   alias la="ll -a"
@@ -26,17 +32,21 @@ else
   alias larth="la -rth"
 fi
 
+# Zoxide
+#---------------------------------------------
 if (( $+commands[zoxide] )); then
   alias cd="z"
 fi
 
-# PYTHON
+# Homebrew
 #---------------------------------------------
 if (( $+commands[brew] )); then
+  # Python
   PYTHON_VERSION='python@3.12'
   export PYTHON_HOME="$(brew --prefix ${PYTHON_VERSION})"
   export PYTHONUSERBASE="${HOME}/.local"
   export PIP_USE_DEPRICATED=html5lib
+  # Path
   pathman PATH "${PYTHON_HOME}/libexec/bin"
   pathman PATH "/usr/local/sbin"
 fi
@@ -51,45 +61,24 @@ for dir in /opt/pkg/*; do
   fi
 done
 
+# Neovim
+# ---------------------------------------------
 if (( $+commands[nvim])); then
   export EDITOR=nvim
   alias vim="nvim"
+  alias vi="nvim"
+  alias v="nvim"
 fi
 
 
+# Wezterm
+# ---------------------------------------------
 if (( $+commands[wezterm])); then
   wz(){
     case "${1}" in
     ls) wezterm cli list ;;
     lsc) wezterm cli list-clients ;;
     *) wezterm "${2}" --help ;;
-    esac
-  }
-fi
-
-if (( $+commands[zellij])); then
-  zj() {
-    in_zj() {
-      [ -n "$ZELLIJ" ] || { echo "Not in a Zellij session"; return 1; }
-    }
-
-    case "${1}" in
-      rt)
-        in_zj || return $?
-        zellij action rename-tab "${2:-$(basename "$PWD")}"
-        ;;
-      rp|rs)
-        in_zj || return $?
-        [ -z "${2}" ] && { echo "$0 $1: Name required"; return 1; }
-        zellij action rename-${1#r} "${2}"
-        ;;
-      *)
-        if [ -z "${1}" ]; then
-            zellij -s "$(basename "$PWD")"
-        else
-            zellij "$@"
-        fi
-        ;;
     esac
   }
 fi
