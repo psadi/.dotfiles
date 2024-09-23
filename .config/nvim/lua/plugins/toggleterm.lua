d49c246ef4957dae3f50a4c96e4596fee038a564
psadi <ps.adithya@icloud.com>
2024-09-23 14:37:06 -0700

    2024-09-23 14:37:06 dotfiles updates
    Former-commit-id: be968eb32569c847f78491bd61c791b3839fb22d
    Former-commit-id: 8670fce9a9899d23ecd5fe2ded3201fd6f96d450
    Former-commit-id: 2f426970c1277270cdaaf63ff5ea980e6b4d8cda
    Former-commit-id: 5421fda778d5f5534b397ab922015f25f04eb71f
    Former-commit-id: d12b80c8de5cd5254d9a5a48be578f0d4225bbe4
    Former-commit-id: c41500e9a8fc9d02b2eeaf37288067254c4177b0
    Former-commit-id: 8317fa04d6dbb7db888cf2efcd2cdffc607a0538

--- .config/nvim/lua/plugins/toggleterm.lua
+++ .config/nvim/lua/plugins/toggleterm.lua
@@ -15,13 +15,19 @@ return {
 
     })
 
-    local Terminal = require('toggleterm.terminal').Terminal
-    local lazygit  = Terminal:new({ cmd = "lazygit", hidden = true })
+    local Terminal   = require('toggleterm.terminal').Terminal
+    local lazygit    = Terminal:new({ cmd = "lazygit", hidden = true })
+    local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true })
 
-    function _lazygit_toggle()
+    function LazyGitToggle()
       lazygit:toggle()
     end
 
-    vim.api.nvim_set_keymap("n", "<leader>lg", "<cmd>lua _lazygit_toggle()<CR>", { noremap = true, silent = true })
+    function LazyDockerToggle()
+      lazydocker:toggle()
+    end
+
+    map("n", "<leader>lg", "<cmd>lua LazyGitToggle()<CR>", { noremap = true, silent = true })
+    map("n", "<leader>ld", "<cmd>lua LazyDockerToggle()<CR>", { noremap = true, silent = true })
   end
 }
