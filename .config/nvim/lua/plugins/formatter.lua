d6a28028a098893445d0d861bdf57b4f7f12ad64
psadi <ps.adithya@icloud.com>
2024-10-01 17:56:49 +0530

    2024-10-01 17:56:49 dotfiles updates
    Former-commit-id: 45778ed3c0ef07d4f99b3c266de6090219b181ae
    Former-commit-id: 2897e7dc8f897dfbc7d23b980f9fb057004bf586
    Former-commit-id: 670d18b1c8412efd0632910f61f6ab32608e6b18
    Former-commit-id: 66bde911ae35a65f1368383f6042edea54963cee
    Former-commit-id: f316500888a13b130d0b7d78e6facd4ed4e35af5
    Former-commit-id: aa66404b45a226866618b767bb00fe50d96e11ae
    Former-commit-id: 9e713adf440acc662cbf900e181e14d98b311342

---
+++ .config/nvim/lua/plugins/formatter.lua
@@ -0,0 +1,40 @@
+return {
+	{
+		"stevearc/conform.nvim",
+		config = function()
+			local conform = require("conform")
+			conform.setup({
+				formatters_by_ft = {
+					html = { "prettier" },
+					css = { "prettier" },
+					json = { "prettier" },
+					yaml = { "prettier" },
+					markdown = { "prettier" },
+					lua = { "stylua" },
+					python = { "ruff_format" },
+					go = { lsp_format = "fallback" },
+				},
+				format_on_save = {
+					async = false,
+					timeout_ms = 1000,
+					lsp_fallback = true,
+					lsp_format = "fallback",
+				},
+			})
+
+			map({ "n", "v" }, "<leader>fmt", function()
+				conform.format({
+					lsp_fallback = true,
+					async = false,
+					timeout_ms = 1000,
+				})
+			end, { desc = "Format file" })
+		end,
+	},
+	{
+		"zapling/mason-conform.nvim",
+		config = function()
+			require("mason-conform").setup()
+		end,
+	},
+}
