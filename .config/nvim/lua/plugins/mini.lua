cc3c9ca40a59c5158cc6a76f1c8fc7717f4ced08
psadi <ps.adithya@icloud.com>
2024-10-07 10:41:56 +0530

    2024-10-07 10:41:56 dotfiles updates
    Former-commit-id: 0ec9d5866a09702706e280b191a0816f4e378b8b
    Former-commit-id: 08b3496ac8ff729e4631d7084ceed4dcfc07b405
    Former-commit-id: 83a67c4765d469222bbacf172e4a88061ef83d4f
    Former-commit-id: 66adffce64fd394b00040a6c4a20fa611f74b9c5
    Former-commit-id: 748a17f25eabaa10e641b8b312e5f2454aa4ad78
    Former-commit-id: 96ca47065f46e8aee879b5f4a94b450b8a4ae2cc
    Former-commit-id: 1a5aa8260c6b67a69d3c74411484e853a6e43c85

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -1,37 +1,44 @@
 return {
-  {
-    'echasnovski/mini.pairs',
-    version = false,
-    config = function()
-      require("mini.pairs").setup()
-    end
-  },
-  {
-    'echasnovski/mini.comment',
-    version = false,
-    config = function()
-      require("mini.comment").setup()
-    end
-  },
-  {
-    'echasnovski/mini.surround',
-    version = false,
-    config = function()
-      require("mini.surround").setup()
-    end
-  },
-  -- {
-  --   'echasnovski/mini.operators',
-  --   version = false,
-  --   config = function()
-  --     require("mini.operators").setup()
-  --   end
-  -- },
-  {
-    'echasnovski/mini.completion',
-    version = false,
-    config = function()
-      require("mini.completion").setup()
-    end
-  },
+	{
+		"echasnovski/mini.pairs",
+		version = false,
+		config = function()
+			require("mini.pairs").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.comment",
+		version = false,
+		config = function()
+			require("mini.comment").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.surround",
+		version = false,
+		config = function()
+			require("mini.surround").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.operators",
+		version = false,
+		config = function()
+			require("mini.operators").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.completion",
+		version = false,
+		config = function()
+			require("mini.completion").setup()
+		end,
+	},
+	{
+		"echasnovski/mini.statusline",
+		version = false,
+		config = function()
+			require("mini.statusline").setup()
+		end,
+	},
 }
