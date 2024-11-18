016260d483eba006ba56eab4f1deccef7efc4d8e
psadi <ps.adithya@icloud.com>
2024-11-19 02:08:59 +0530

    2024-11-19 02:08:59 dotfiles updates

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -14,15 +14,16 @@ config.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
 config.color_scheme = "Gruvbox dark, hard (base16)"
 config.tab_max_width = 20
 config.use_fancy_tab_bar = false
-config.enable_tab_bar = false
+config.enable_tab_bar = true
 config.enable_scroll_bar = false
 config.window_padding = {
-	left = 0,
+	-- left = 0,
 	right = 0,
 	top = 0,
 	bottom = 0,
 }
 config.window_close_confirmation = "NeverPrompt"
+-- config.window_decorations = "TITLE | RESIZE"
 -- -----------------------------------------------------
 -- Keybindings
 -- -----------------------------------------------------
