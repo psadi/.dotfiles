0cbbdecb55157d9ce0b5d55611db84e61d0100ce
psadi <ps.adithya@icloud.com>
2024-12-06 12:31:23 +0530

    2024-12-06 12:31:23 dotfiles updates

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,18 +1,11 @@
 return {
-	-- "deparr/tairiki.nvim",
-	-- lazy = false,
-	-- priority = 1000,
-	-- config = function()
-	-- 	require("tairiki").setup({
-	-- 		transparent = true,
-	-- 	})
-	-- 	require("tairiki").load()
-	-- end,
-	"Mofiqul/vscode.nvim",
+	"deparr/tairiki.nvim",
+	lazy = false,
+	priority = 1000,
 	config = function()
-		require("vscode").setup({
+		require("tairiki").setup({
 			transparent = true,
 		})
-		vim.cmd.colorscheme("vscode")
+		require("tairiki").load()
 	end,
 }
