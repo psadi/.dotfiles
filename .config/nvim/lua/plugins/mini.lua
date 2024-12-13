5563dd9b612fbcc8ee42193d9ee0b356bd53279b
psadi <ps.adithya@icloud.com>
2024-12-13 10:51:37 +0530

    2024-12-13 10:51:37 dotfiles updates

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -31,7 +31,9 @@ return {
 		"echasnovski/mini.indentscope",
 		version = false,
 		config = function()
-			require("mini.indentscope").setup()
+			require("mini.indentscope").setup({
+				delay = 0,
+			})
 		end,
 	},
 	{
