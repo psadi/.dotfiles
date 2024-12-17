6f0614cd95c1866ef8eafe3b4ec52dd043b48c4c
psadi <ps.adithya@icloud.com>
2024-12-17 19:39:13 +0530

    2024-12-17 19:39:13 dotfiles updates

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -5,6 +5,13 @@ return {
 	config = function()
 		require("catppuccin").setup({
 			flavour = "mocha",
+			integrations = {
+				blink_cmp = true,
+				mini = {
+					enabled = true,
+					indentscope_color = "lavender",
+				},
+			},
 		})
 		vim.cmd.colorscheme("catppuccin")
 	end,
