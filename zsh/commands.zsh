e7ec8bf174d741caaf47c7334622d8adeb78102c
psadi <ps.adithya@icloud.com>
2025-01-02 16:16:39 +0530

    dotfiles update

--- zsh/commands.zsh
+++ zsh/commands.zsh
@@ -25,7 +25,7 @@ if (( $+commands[bat] )); then
   alias cat="bat -p"
   export MANPAGER="bat -l man -p"
   export MANROFFOPT='-c'
-  export BAT_THEME="base16"
+  export BAT_THEME="tokyonight_night"
 fi
 
 # Eza
