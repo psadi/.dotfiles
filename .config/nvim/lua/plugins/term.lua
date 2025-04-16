fceb1b07c10ac52e281e4210fe84cf3a19561b31
psadi <mail@adithyaps.xyz>
2025-04-17 04:03:49 +0530

    .dotfiles update

--- .config/nvim/lua/plugins/term.lua
+++ .config/nvim/lua/plugins/term.lua
@@ -13,29 +13,12 @@ return {
 		})
 
 		local Terminal = require("toggleterm.terminal").Terminal
-		local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-		local lazydocker = Terminal:new({ cmd = "lazydocker", hidden = true })
-		local k9s = Terminal:new({ cmd = "k9s --logoless --headless --readonly --crumbsless", hidden = true })
-		local yazi = Terminal:new({ cmd = "yazi", hidden = true })
+		local git = Terminal:new({ cmd = "gitu", hidden = true })
 
-		function LazyGitToggle()
-			lazygit:toggle()
+		function GitToggle()
+			git:toggle()
 		end
 
-		function LazyDockerToggle()
-			lazydocker:toggle()
-		end
-
-		function K9sToggle()
-			k9s:toggle()
-		end
-
-		function YaziToggle()
-			yazi:toggle()
-		end
-		map("n", "<leader>lg", "<cmd>lua LazyGitToggle()<CR>", { noremap = true, silent = true })
-		map("n", "<leader>ld", "<cmd>lua LazyDockerToggle()<CR>", { noremap = true, silent = true })
-		map("n", "<leader>k9", "<cmd>lua K9sToggle()<CR>", { noremap = true, silent = true })
-		map("n", "<leader>yz", "<cmd>lua YaziToggle()<CR>", { noremap = true, silent = true })
+		map("n", "<leader>gg", "<cmd>lua GitToggle()<CR>", { noremap = true, silent = true })
 	end,
 }
