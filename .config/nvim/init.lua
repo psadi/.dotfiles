e9e26035e544109fbb56e97c811cd1cac8c58302
psadi <mail@adithyaps.xyz>
2025-08-13 07:44:50 +0530

    .dotfiles update

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,31 +1,103 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
+-- Opts
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
-vim.opt.rtp:prepend(lazypath)
+vim.cmd.set("expandtab")
+vim.cmd.set("tabstop=4")
+vim.cmd.set("softtabstop=4")
+vim.cmd.set("shiftwidth=4")
+vim.cmd.set("relativenumber")
+vim.cmd.set("colorcolumn=80")
+vim.cmd.set("shortmess+=I")
+vim.cmd.set("t_Co=256")
+vim.cmd.set("termguicolors")
+vim.cmd.set("completeopt+=noselect")
+vim.opt.list = true
+vim.opt.listchars = { tab = "▸ ", trail = "•", extends = ">", precedes = "<", nbsp = "␣" }
+vim.opt.winborder = "rounded"
+vim.opt.statusline = "%<%f %l,%c%V"
 
-require("options")
-require("lazy").setup({
-	spec = {
-		{ import = "plugins" },
+-- Install Packages
+vim.pack.add({
+	-- "https://github.com/echasnovski/mini.completion",
+	"https://github.com/echasnovski/mini.icons",
+	"https://github.com/echasnovski/mini.trailspace",
+	"https://github.com/ibhagwan/fzf-lua",
+	"https://github.com/nvim-treesitter/nvim-treesitter",
+	"https://github.com/rebelot/kanagawa.nvim",
+	"https://github.com/stevearc/oil.nvim",
+})
+
+-- ColorScheme
+vim.cmd("colorscheme kanagawa")
+
+-- Load Plugins
+-- require("mini.completion").setup()
+require("mini.icons").setup()
+require("mini.trailspace").setup()
+require("fzf-lua").setup({})
+require("oil").setup({
+	delete_to_trash = true,
+	view_options = {
+		show_hidden = true,
 	},
-	performance = {
-		rtp = {
-			disabled_plugins = {
-				"gzip",
-				"tarPlugin",
-				"tohtml",
-				"tutor",
-				"zipPlugin",
-			},
-		},
+})
+require("nvim-treesitter.configs").setup({
+	auto_install = true,
+	sync_install = false,
+	highlight = { enable = true },
+	indent = { enable = true },
+})
+
+-- Keyvim.keymap.sets
+vim.keymap.set("n", "<c-l>", ":nohlsearch<CR>")
+vim.keymap.set("n", "<leader>bd", "<CMD>bd<CR>", { silent = true })
+vim.keymap.set("n", "<leader>BD", "<CMD>bd!<CR>", { silent = true })
+vim.keymap.set("n", "<leader>qq", "<CMD>q!<CR>", { silent = true })
+vim.keymap.set("n", "<leader>%", "<CMD>source %<CR>", { desc = "Source Config" })
+vim.api.nvim_set_keymap("n", "<leader>w", ":set list!<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<leader>ff", "<CMD>FzfLua files<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fb", "<CMD>FzfLua buffers<CR>", { silent = true })
+vim.keymap.set("n", "<leader>fg", "<CMD>FzfLua grep_visual<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gC", "<CMD>FzfLua git_commits<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gS", "<CMD>FzfLua git_status<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gB", "<CMD>FzfLua git_branches<CR>", { silent = true })
+vim.keymap.set("n", "<leader>gT", "<CMD>FzfLua git_tags<CR>", { silent = true })
+vim.keymap.set("n", "<leader>H", "<CMD>FzfLua search_history<CR>", { silent = true })
+vim.keymap.set("n", "<leader>K", "<CMD>FzfLua keyvim.keymap.sets<CR>", { silent = true })
+vim.keymap.set("n", "_", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<leader>tr", "<CMD>lua MiniTrailspace.trim()<CR>", { silent = true })
+vim.keymap.set("n", "<leader>ltr", "<CMD>lua MiniTrailspace.trim_last_lines()<CR>", { silent = true })
+
+-- Lsp
+vim.lsp.enable({
+	"lua_ls",
+	"ruff",
+	"rust_analyzer",
+	"terraformls",
+	"ansiblels.lua",
+	"yamlls",
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+		if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
+			vim.opt.completeopt = { "menu", "menuone", "noinsert", "fuzzy", "popup" }
+			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+			vim.keymap.set("i", "<C-Space>", function()
+				vim.lsp.completion.get()
+			end)
+		end
+	end,
+})
+
+vim.keymap.set("n", "<leader>fmt", vim.lsp.buf.format)
+
+-- Diagnostics
+vim.diagnostic.config({
+	-- virtual_lines = true,
+	virtual_lines = {
+		current_line = true,
 	},
 })
---
+++ .config/nvim/lsp/terraformls.lua
@@ -0,0 +1,5 @@
+return {
+	cmd = { "terraform-lsp" },
+	filetypes = { "terraform" },
+	root_markers = { ".git" },
+}
