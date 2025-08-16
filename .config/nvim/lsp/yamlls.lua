23ab6f55ebd319c833806ecaf9c02af559266f83
psadi <mail@adithyaps.xyz>
2025-08-16 09:19:03 +0530

    .dotfiles update

--- .config/nvim/lsp/yamlls.lua
+++
@@ -1,8 +0,0 @@
-return {
-	cmd = { "yaml-language-server", "--stdio" },
-	filetypes = { "yaml", "yaml.docker-compose", "yaml.gitlab", "yaml.helm-values" },
-	root_markers = { ".git" },
-	settings = {
-		redhat = { telemetry = { enabled = false } },
-	},
-}
