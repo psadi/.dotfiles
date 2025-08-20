34c0be8623ed98bac94e762fb8fe8fadd5d9686d
psadi <mail@adithyaps.xyz>
2025-08-20 14:23:23 +0530

    .dotfiles update

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -10,7 +10,7 @@ return {
 		event = "VeryLazy",
 		config = function()
 			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "ruff", "pyright", "terraformls", "gopls" },
+				ensure_installed = { "lua_ls", "ruff", "pyright", "terraformls", "gopls", "rust_analyzer" },
 			})
 		end,
 	},
