b81e0fd7f502edfb3abffb3595b44d0f081d78b2
psadi <mail@adithyaps.xyz>
2025-08-20 22:42:54 +0530

    .dotfiles update

--- .config/nvim/lua/plugins/mini.lua
+++ .config/nvim/lua/plugins/mini.lua
@@ -17,7 +17,12 @@ return {
 		"echasnovski/mini.indentscope",
 		version = false,
 		config = function()
-			require("mini.indentscope").setup()
+			require("mini.indentscope").setup({
+				draw = {
+					animation = require("mini.indentscope").gen_animation.none(),
+				},
+				symbol = "│",
+			})
 		end,
 	},
 	{
