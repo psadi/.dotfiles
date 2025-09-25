58735aeeda50a6bb5f64454665672e5a82d1485b
psadi <mail@adithyaps.xyz>
2025-09-26 00:20:13 +0530

    .dotfiles update

--- bootstrap.sh
+++ bootstrap.sh
@@ -216,8 +216,8 @@ install_python_deps
 install_zap
 configure_user_shell
 # configure_font
-configure_theme
+#configure_theme
 stow_link
-gnome_tweaks
+#gnome_tweaks
 configure_systemd_services
-general_system_tweaks
+#general_system_tweaks
